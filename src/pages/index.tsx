4f63ecddf07562d912d534f1301997e095861e53
arthurkoblet210 <arthurkoblet210@gmail.com>
2024-02-25 23:29:15 -0500

    Add import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';
 
-import Page from '../components/Layout/Page';
+
 import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
@@ -10,7 +10,7 @@ import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
 import {homePageMeta} from '../data/data';
-
+import Page from '../components/Layout/Page';
 // eslint-disable-next-line react-memo/require-memo
 const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
 
