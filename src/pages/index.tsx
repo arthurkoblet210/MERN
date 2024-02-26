d70bc5cef0eb5466b61dac4774e3afa8f19b023d
arthurkoblet210 <arthurkoblet210@gmail.com>
2024-02-26 02:12:58 -0500

    #7 fix it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';
+import Portfolio from '../components/Sections/Portfolio';
+import Resume from '../components/Sections/Resume';
+import Testimonials from '../components/Sections/Testimonials';
+import {homePageMeta} from '../data/data';
 
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
-import Portfolio from '../components/Sections/Portfolio';
-import Resume from '../components/Sections/Resume';
-import Testimonials from '../components/Sections/Testimonials';
-import {homePageMeta} from '../data/data';
+
 
 // eslint-disable-next-line react-memo/require-memo
 const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
